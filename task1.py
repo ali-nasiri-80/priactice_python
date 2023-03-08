6e0a9c83f0d2125f797ee9cc699d2871c4f80c43
ali-nasiri-80 <alinasiri576@gmail.com>
2023-03-08 17:49:19 +0330

    Add some exercises

--- task1.py
+++ task1.py
@@ -74,5 +74,67 @@
 #         f1 = f2
 #         f2 = f3
 #         i = i + 1
-# -----------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------
+#6-Write a program that takes a string and creates a space between each character
+# s = input("Enter the string:")
+# for i in s:
+#     print(i,end="  ")
+#---------------------------------------------------------------------------------------
+#7-A program that reads the user's birth year and the current year from input, determines how many years,
+#months, days, hours, minutes, and seconds he has lived.
 
+# byy = int(input("Enter brith data(year):"))
+# bmm = int(input("Enter brith data(month):"))
+# bdd = int(input("Enter brth data(day):"))
+# cyy = int(input("Enter current data(year):"))
+# cmm = int(input("Enter current data(month):"))
+# cdd = int(input("Enter current data(day):"))
+# if cdd < bdd:
+#     cmm-=1
+#     cdd += 30
+# day = cdd - bdd
+# if cmm < bmm:
+#     cyy -= 1
+#     cdd += 30
+# month = cmm - bmm
+# year = cyy - byy
+# days = day + month * 30 + year * 36
+# hh = day + 24
+# mm = hh * 60
+# ss = mm * 60
+# print("Old is :{0}/{1}/{2}",year,month,day)
+# print("Hour is(hh:mm:ss):{0}:{1}:{2}",hh,mm,ss)
+#---------------------------------------------------------------------------------------------------------------
+#8-Write a program that receives three numbers from the user and sorts them in ascending order
+#a = int(input("Enter the number 1:"))
+#b = int(input("Enter the number 2:"))
+#c = int(input("Enter the number 3:"))
+#
+#if  a > b :
+#    temp = a
+#    a = b 
+#    b = temp
+#if a > c :
+#    temp = a
+#    a = c 
+#    c = temp
+#if b > c:
+#    temp = b
+#    b = c 
+#    c = temp
+#print("sorted is ",a,b,c)
+#-------------------------------------------------------------------------------------------------------------------
+#9-Write a program that performs the Fibonacci series with a recursive function
+# def fibo(n):
+#     if (n==1):
+#         return (1)
+#     if (n==2):
+#         return (1)
+#     return (fibo(n-2) + fibo(n-1))
+# def main():
+#     n = int(input("Enter n sri:"))
+#     print("Result is :")
+#     for i in range(1 , n+1):
+#         print(fibo(i),end=" ")
+# main()
+#------------------------------------------------------------------------------------------------------------------
